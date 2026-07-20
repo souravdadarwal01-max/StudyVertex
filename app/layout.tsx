2755d718be98d4b4a9034dacecd62a2b2bad478c
souravdadarwal01-max <souravdadarwal01@gmail.com>
2026-07-20 10:16:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,11 +18,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
+
       <body className={inter.className}>
+
+        <Navbar />
+
         {children}
+
       </body>
+
     </html>
   );
 }
